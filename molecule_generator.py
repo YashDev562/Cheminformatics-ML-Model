1b8a87f1f2f49175de1445a55a6e343b5d25b93e
YashDev562 <yashgd562@gmail.com>
2026-01-23 01:12:34 +0530

    Only the molecule generator (adjacency matrix part is done), the molecule conversion still needs to be done

--- molecule_generator.py
+++ molecule_generator.py
@@ -1,31 +1,67 @@
 import pandas as pd
 import numpy as np
 import random as rm
+from rdkit import Chem
+from enum import Enum
+import random as rd
 
-def gen_structure(n_of_carbon,n_of_hetero = 0) :
-  mol_matrix = np.zeros((n_of_carbon,n_of_carbon),dtype=int)
-  print(f"Original Matrix \n {mol_matrix}")
+class HeteroAtoms(Enum) :
+  Nitrogen = "N"
+  Oxygen = "O"
 
-  for row in range(0,n_of_carbon) :
+# For heteroatoms, take in a dictionary of atom:atomCount, give out a tuple of (row of heteroatoms as columns of matrix,column of heteroatoms)
+# [O:4,N:3]
+def gen_structure(n_of_carbon,heteroatom_dict) :
+  matrix_column = []
+  for u in range(0,n_of_carbon) :
+    matrix_column.append("C")
+
+  for element,count in heteroatom_dict.items() :
+    if element in HeteroAtoms :
+      for i in range(1,count+1) :
+        matrix_column.append(element)
+    else :
+      print("Element doesnt exist")
+      return
+      
+  mol_matrix = np.zeros((len(matrix_column),len(matrix_column)),dtype=int)
+
+  rd.shuffle(matrix_column)
+
+  if(len(matrix_column) != mol_matrix.shape[0]) :
+    print()
+  for row in range(0,len(matrix_column)) :
     for col in range(0,row+1) :
       mol_matrix[row][col] = rm.randint(0,1)
   
 
-  for row in range(0,n_of_carbon) :
-    for col in range(row,n_of_carbon) :
+  for row in range(0,len(matrix_column)) :
+    for col in range(row,len(matrix_column)) :
       mol_matrix[row][col] = mol_matrix[col][row]
       if row == col :
         mol_matrix[row][col] = 0
 
-  return mol_matrix
+  molecule = (mol_matrix,matrix_column)
+  return molecule
 
-def gen_no_of_structure(n_of_structures,n_of_carbons,n_of_heteroatoms=0) :
+def gen_no_of_structure(n_of_structures,n_of_carbons,heteroAtom_dict) :
   mol_dict = {}
   for i in range(1,n_of_structures + 1) :
-    mol = gen_structure(n_of_carbons,n_of_heteroatoms)
+    mol = gen_structure(n_of_carbons,heteroAtom_dict)
     mol_dict[f"mol_{i}"] = mol
   return mol_dict
 
+
+def matrix_to_structure(molec_tuple) :
+  adj_matrix = molec_tuple[0]
+  hetero_info = molec_tuple[1]
+
+  molec_df = pd.DataFrame(adj_matrix,index=)
+  for i in range(0,adj_matrix.shape[0]) :
+    for j in range (0,i) :
+      
+
 if __name__ == "__main__" :
-  first_mol_batch = gen_no_of_structure(10,5)
+  heteroAtom_Dict = {"O":2,"N":3}
+  first_mol_batch = gen_no_of_structure(10,5,heteroAtom_Dict)
   print(first_mol_batch)
