f57b0dfd9ac30933f3ce1568948e5733533ad84d
YashDev562 <yashgd562@gmail.com>
2026-01-24 11:19:12 +0530

    Finally added the RDKit logic to convert the adjacency matrices to actual chemical molecules. Currently outputs 2D molecules in a PNG file. Avoid too any heteroatoms or generating too many structures

--- molecule_generator.py
+++ molecule_generator.py
@@ -3,6 +3,9 @@ import numpy as np
 import random as rm
 from rdkit import Chem
 from enum import Enum
+from rdkit.Chem import rdmolops
+from rdkit.Chem import Draw
+from rdkit.Chem import rdCoordGen
 import random as rd
 
 class HeteroAtoms(Enum) :
@@ -29,17 +32,25 @@ def gen_structure(n_of_carbon,heteroatom_dict) :
   rd.shuffle(matrix_column)
 
   if(len(matrix_column) != mol_matrix.shape[0]) :
-    print()
+    print("Given number of atoms out of bounds of vertex adjacency matrix, try again")
+    return
+  
   for row in range(0,len(matrix_column)) :
     for col in range(0,row+1) :
       mol_matrix[row][col] = rm.randint(0,1)
-  
 
   for row in range(0,len(matrix_column)) :
-    for col in range(row,len(matrix_column)) :
+    mol_matrix[row][row] = 0
+    if np.sum(mol_matrix[row]) == 0 :
+      col_to_set = rm.choice([i for i in range(mol_matrix.shape[1]) if i != row])
+      if col_to_set > row :
+        mol_matrix[col_to_set][row] = 1
+      else :
+        mol_matrix[row][col_to_set] = 1
+
+  for row in range(0,len(matrix_column)) :
+    for col in range(row+1,len(matrix_column)) :
       mol_matrix[row][col] = mol_matrix[col][row]
-      if row == col :
-        mol_matrix[row][col] = 0
 
   molecule = (mol_matrix,matrix_column)
   return molecule
@@ -51,17 +62,46 @@ def gen_no_of_structure(n_of_structures,n_of_carbons,heteroAtom_dict) :
     mol_dict[f"mol_{i}"] = mol
   return mol_dict
 
-'''
 def matrix_to_structure(molec_tuple) :
   adj_matrix = molec_tuple[0]
   hetero_info = molec_tuple[1]
 
-  molec_df = pd.DataFrame(adj_matrix,index=)
+  mol = Chem.RWMol()
+  for atom_symbol in hetero_info :
+    atom = Chem.Atom(atom_symbol)
+    mol.AddAtom(atom)
+
   for i in range(0,adj_matrix.shape[0]) :
-    for j in range (0,i) :
-      
-'''
+    for j in range (0,i+1) :
+      if (adj_matrix[i][j] == 1) :
+        mol.AddBond(i,j,Chem.BondType.SINGLE)
+      if (adj_matrix[i][j] == 2) :
+        mol.AddBond(i,j,Chem.BondType.DOUBLE)
+      if (adj_matrix[i][j] == 3) :
+        mol.AddBond(i,j,Chem.BondType.TRIPLE)     
+
+  final_mol=mol.GetMol()
+  try :
+    rdmolops.SanitizeMol(final_mol)
+    rdCoordGen.AddCoords(final_mol)
+    return final_mol
+  except (Chem.rdchem.AtomValenceException) :
+    print("Molecule doesnt satisfy the laws of chemistry")
+    return
+
 if __name__ == "__main__" :
-  heteroAtom_Dict = {"O":2,"N":3}
-  first_mol_batch = gen_no_of_structure(10,5,heteroAtom_Dict)
+  heteroAtom_Dict = {"O":1,"N":0}
+  first_mol_batch = gen_no_of_structure(200,4,heteroAtom_Dict)
   print(first_mol_batch)
+  final_mols = []
+  for mol in first_mol_batch.values() :
+    actual_mol = matrix_to_structure(mol)
+    if actual_mol not in final_mols and actual_mol is not None :
+      final_mols.append(actual_mol)
+  print(f"No of mols finally = {len(final_mols)}")
+  if final_mols:
+    mols_display = Draw.MolsToImage(final_mols)
+    mols_display.show()
+  else:
+    print("The molecule list is empty")
+  
